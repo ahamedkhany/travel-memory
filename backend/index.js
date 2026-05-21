8b7be05676b646adfbab24288a43e68f1d42a691
root <root@ip-172-31-21-115.ec2.internal>
2026-05-21 16:54:09 +0000

    index js code change to test health

--- backend/index.js
+++ backend/index.js
@@ -10,10 +10,14 @@ app.use(cors())
 
 const tripRoutes = require('./routes/trip.routes')
 
-app.use('/trip', tripRoutes) // http://localhost:3001/trip --> POST/GET/GET by ID
+app.use('/trip', tripRoutes) // http://localhost:3000/trip --> POST/GET/GET by ID
 
 app.get('/hello', (req,res)=>{
     res.send('Hello World!')
+})
+
+app.get("/", (req, res) => {
+    res.send("Backend running successfully")
 })
 
 app.listen(PORT, ()=>{
